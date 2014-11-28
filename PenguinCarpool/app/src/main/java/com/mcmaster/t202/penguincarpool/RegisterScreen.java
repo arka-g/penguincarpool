4ae3ee1e99a316afa4a2a98763994840784fd573
Arka Ganguli <arkaganguli1@gmail.com>
2014-11-28 13:31:38 -0500

    register user completed

--- PenguinCarpool/app/src/main/java/com/mcmaster/t202/penguincarpool/RegisterScreen.java
+++ PenguinCarpool/app/src/main/java/com/mcmaster/t202/penguincarpool/RegisterScreen.java
@@ -1,17 +1,44 @@
 package com.mcmaster.t202.penguincarpool;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.BasicHttpContext;
+import org.apache.http.protocol.HttpContext;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class RegisterScreen extends Activity {
+public class RegisterScreen extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
+        findViewById(R.id.regSubmitInfo).setOnClickListener(this);
     }
 
 
@@ -22,6 +49,78 @@ public class RegisterScreen extends Activity {
         return true;
     }
 
+    public void onClick(View arg0) {
+        Button b = (Button) findViewById(R.id.regSubmitInfo);
+        b.setClickable(false);
+        //execute get and post
+        new LongRunningGetIO().execute();
+    }
+
+
+    private class LongRunningGetIO extends AsyncTask<Void, Void, String> {
+        @Override
+
+        protected String doInBackground(Void... params) {
+
+
+            EditText first_name = (EditText) findViewById(R.id.regFirstName);
+            EditText last_name = (EditText) findViewById(R.id.regLastName);
+            EditText email = (EditText) findViewById(R.id.regEmail);
+            //EditText username = (EditText) findViewById(R.id.username);
+            EditText password = (EditText) findViewById(R.id.regPassword);
+
+            String str1 = first_name.getText().toString();
+            String str2 = last_name.getText().toString();
+            String str3 = email.getText().toString();
+            //String str4 = username.getText().toString();
+            String str5 = password.getText().toString();
+
+            HttpClient httpClient = new DefaultHttpClient();
+            // replace with your url
+            HttpPost httpPost = new HttpPost("http://10.0.2.2/penguin-carpool/public/save");
+
+            //Post Data
+            List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(4);
+            nameValuePair.add(new BasicNameValuePair("first_name", str1));
+            nameValuePair.add(new BasicNameValuePair("last_name", str2));
+            nameValuePair.add(new BasicNameValuePair("email", str3));
+//                nameValuePair.add(new BasicNameValuePair("username", str4));
+            nameValuePair.add(new BasicNameValuePair("password", str5));
+
+
+            //Encoding POST data
+            try {
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+            } catch (UnsupportedEncodingException e) {
+                // log exception
+                e.printStackTrace();
+            }
+
+            //making POST request.
+            try {
+                HttpResponse response = httpClient.execute(httpPost);
+                // write response to log
+                Log.d("Http Post Response:", response.toString());
+            } catch (ClientProtocolException e) {
+                // Log exception
+                Log.d("Client Exception", e.toString());
+                e.printStackTrace();
+            } catch (IOException e) {
+                // Log exception
+                Log.d("IO Exception", e.toString());
+                e.printStackTrace();
+            }
+            //i dont need this..
+            return str1;
+        }
+
+        protected void onPostExecute(String results) {
+            Button b = (Button) findViewById(R.id.regSubmitInfo);
+            b.setClickable(true);
+        }
+
+
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will

eb0b07f9d118cea71f45dcf7acec954f4c43afc2
Arka Ganguli <arkaganguli1@gmail.com>
2014-11-30 13:41:17 -0500

    push idle screen changes

--- PenguinCarpool/app/src/main/java/com/mcmaster/t202/penguincarpool/IdleScreen.java
+++ PenguinCarpool/app/src/main/java/com/mcmaster/t202/penguincarpool/IdleScreen.java
@@ -3,9 +3,11 @@ package com.mcmaster.t202.penguincarpool;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 
 public class IdleScreen extends Activity {
@@ -40,5 +42,49 @@ public class IdleScreen extends Activity {
         Intent intent = new Intent("com.google.zxing.client.android.SCAN");
         intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
         startActivityForResult(intent, 0);
+
     }
+
+    /* @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case IntentIntegrator.REQUEST_CODE:
+                IntentResult scanResult = IntentIntegrator.parseActivityResult(
+                        requestCode, resultCode, data);
+                if (scanResult == null) {
+                    return;
+                }
+                final String result = scanResult.getContents(); // Your result
+
+                if (result != null) {
+                    //System.out.print("Your result is: " + result);
+                    //Log.d("Your result is ", result);
+
+                }
+                break;
+            default:
+        }
+    } */
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 0) {
+            if (resultCode == RESULT_OK) {
+                String contents = intent.getStringExtra("SCAN_RESULT");
+                String format = intent.getStringExtra("SCAN_RESULT_FORMAT");
+                // Handle successful scan
+                Toast toast = Toast.makeText(this, "Content:" + contents + " Format:" + format , Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP, 25, 400);
+                toast.show();
+            } else if (resultCode == RESULT_CANCELED) {
+                // Handle cancel
+                Toast toast = Toast.makeText(this, "Scan was Cancelled!", Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.TOP, 25, 400);
+                toast.show();
+
+            }
+        }
+    }
+
 }

27bc38517f2ab09448330ff1cd44db710c271220
Nick Lelievre <nmjlelievre@gmail.com>
2014-12-01 19:49:01 -0500

    Should be good now

---
+++ mapstest/app/src/main/java/com/example/mykolasomov/mapstest/MapScreen.java
@@ -0,0 +1,19 @@
+package com.example.mykolasomov.mapstest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.mykolasomov.mapstest.R;
+
+/**
+ * Created by Arka on 2014-11-30.
+ */
+public class MapScreen extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_screen);
+
+    }
+
+}

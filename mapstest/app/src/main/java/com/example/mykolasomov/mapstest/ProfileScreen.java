5570684c77a49c13b197369837540b65c0a181e0
Arka Ganguli <arkaganguli1@gmail.com>
2014-12-03 15:03:55 -0500

    delete user implemented

--- mapstest/app/src/main/java/com/example/mykolasomov/mapstest/ProfileScreen.java
+++ mapstest/app/src/main/java/com/example/mykolasomov/mapstest/ProfileScreen.java
@@ -1,8 +1,11 @@
 package com.example.mykolasomov.mapstest;
 
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,6 +13,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ProfileScreen extends LoginScreen {
@@ -18,6 +36,8 @@ public class ProfileScreen extends LoginScreen {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_screen);
+        findViewById(R.id.deletebtn).setOnClickListener(this);
+
         TextView textView = (TextView) findViewById(R.id.userFirstName);
         TextView textView1 = (TextView) findViewById(R.id.userLastName);
         TextView textView2 = (TextView) findViewById(R.id.userEmail);
@@ -28,10 +48,62 @@ public class ProfileScreen extends LoginScreen {
         textView1.setText(lastname);
         textView2.setText(email);
         textView3.setText(message);
-        //hack for now. come back to it and add a real rating system
         ratinguser.setRating(rating);
     }
+    public void onClick(View arg0) {
+        Button b = (Button) findViewById(R.id.deletebtn);
+        b.setClickable(false);
+        //execute get and post
+        new DeleteIO().execute();
+    }
 
+
+    private class DeleteIO extends AsyncTask<Void, Void, String> {
+        @Override
+
+        protected String doInBackground(Void... params) {
+            // replace with your url
+            HttpPost httpPost = new HttpPost("http://10.0.2.2/penguin-carpool/public/delete");
+//            HttpPost httpPost = new HttpPost("http://172.17.31.169/penguin-carpool/public/save");
+            //Post Data
+            List<NameValuePair> nameValuePair = new ArrayList<NameValuePair>(1);
+            nameValuePair.add(new BasicNameValuePair("id", Integer.toString(id)));
+
+            //Encoding POST data
+            try {
+                httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+            } catch (UnsupportedEncodingException e) {
+                // log exception
+                e.printStackTrace();
+            }
+
+            //making POST request.
+            try {
+                HttpResponse response = httpClient.execute(httpPost);
+                // write response to log
+                Log.d("Http Post Response:", response.toString());
+            } catch (ClientProtocolException e) {
+                // Log exception
+                Log.d("Client Exception", e.toString());
+                e.printStackTrace();
+            } catch (IOException e) {
+                // Log exception
+                Log.d("IO Exception", e.toString());
+                e.printStackTrace();
+            }
+            //this is useless
+            return "nothing";
+        }
+
+        protected void onPostExecute(String results) {
+            Button b = (Button) findViewById(R.id.deletebtn);
+            b.setClickable(true);
+            // Return to LoginScreen
+            startActivity(new Intent(ProfileScreen.this, LoginScreen.class));
+        }
+
+
+    }
     /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
